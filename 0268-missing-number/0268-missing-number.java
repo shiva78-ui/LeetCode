92204be8aa1b8791ba622a8f431539a9d63f9618
shiva78-ui <shivayadavmy8570238@gmail.com>
2026-08-27 23:57:17 +0530

    Time: 0 ms (100%), Space: 47.4 MB (48.99%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+
+        int actual = (n * (n + 1)) / 2;
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+        }
+
+        return actual - sum;
+    }
+}
